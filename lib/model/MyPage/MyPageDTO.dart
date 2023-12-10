26fe8feddee2c83f95ab84ae634cf317ab8095ed
MJ <alswns2631@cau.ac.kr>
2023-12-10 23:50:14 +0900

    [UPDATE]: MyPage DTO 추가

---
+++ lib/model/MyPage/MyPageDTO.dart
@@ -0,0 +1,34 @@
+import '../matchinginfo/NEW/MemberInfo.dart';
+
+class MyPageDTO {
+  final MemberInfoDTO memberInfoDTO;
+  final String myIntroduceTitle;
+  final String myIntroduceContents;
+
+  MyPageDTO({
+    required this.memberInfoDTO,
+    required this.myIntroduceTitle,
+    required this.myIntroduceContents,
+  });
+
+  factory MyPageDTO.fromJson(Map<String, dynamic> json) {
+    return MyPageDTO(
+      memberInfoDTO: MemberInfoDTO.fromJson(json['memberInfoDTO']),
+      myIntroduceTitle: json['myIntroduceTitle'],
+      myIntroduceContents: json['myIntroduceContents'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'memberInfoDTO': memberInfoDTO.toJson(),
+      'myIntroduceTitle': myIntroduceTitle,
+      'myIntroduceContents': myIntroduceContents,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'MyPageDTO{memberInfoDTO: $memberInfoDTO, myIntroduceTitle: $myIntroduceTitle, myIntroduceContents: $myIntroduceContents}';
+  }
+}
