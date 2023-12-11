db4f2ee2e0a2dae3eeeee987f384669a3cd1db0a
이주형 JuhyeongLee <smilebank7@naver.com>
2023-12-11 10:02:53 +0900

    Merge branch 'main' of /Users/inshjh/Documents/GitHub/TravelWith_front with conflicts.

--- lib/pages/matching/search/conditionSearch.dart
+++ lib/pages/matching/search/conditionSearch.dart
@@ -20,85 +20,83 @@ class _ConditionSearchState extends ConsumerState<ConditionSearch> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: AlertDialog(
-        content: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: startDateController,
-                decoration: InputDecoration(labelText: '시작 날짜'),
-                onTap: () async {
-                  DateTime? selectedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2101),
-                  );
-                  if (selectedDate != null) {
-                    startDateController.text =
-                    selectedDate.toLocal().toString().split(' ')[0];
-                  }
-                },
+    return AlertDialog(
+      content: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: startDateController,
+              decoration: InputDecoration(labelText: 'Start date'),
+              onTap: () async {
+                DateTime? selectedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101),
+                );
+                if (selectedDate != null) {
+                  startDateController.text =
+                  selectedDate.toLocal().toString().split(' ')[0];
+                }
+              },
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: endDateController,
+              decoration: InputDecoration(labelText: 'End Date'),
+              onTap: () async {
+                DateTime? selectedDate = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2000),
+                  lastDate: DateTime(2101),
+                );
+                if (selectedDate != null) {
+                  endDateController.text =
+                  selectedDate.toLocal().toString().split(' ')[0];
+                }
+              },
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: keywordController,
+              decoration: InputDecoration(labelText: 'Search Keyword'),
+            ),
+            SizedBox(height: 16.0),
+            Text('Expenses: ${minAmount.toStringAsFixed(0)}Won - ${maxAmount.toStringAsFixed(0)}Won'),
+            RangeSlider(
+              values: RangeValues(minAmount, maxAmount),
+              onChanged: (RangeValues values) {
+                setState(() {
+                  minAmount = values.start;
+                  maxAmount = values.end;
+                });
+              },
+              min: 0,
+              max: 1000000,
+              divisions: 100,
+              labels: RangeLabels(
+                minAmount.toStringAsFixed(2),
+                maxAmount.toStringAsFixed(2),
               ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: endDateController,
-                decoration: InputDecoration(labelText: '종료 날짜'),
-                onTap: () async {
-                  DateTime? selectedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2101),
-                  );
-                  if (selectedDate != null) {
-                    endDateController.text =
-                    selectedDate.toLocal().toString().split(' ')[0];
-                  }
-                },
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: keywordController,
-                decoration: InputDecoration(labelText: '검색어'),
-              ),
-              SizedBox(height: 16.0),
-              Text('금액 범위: ${minAmount.toStringAsFixed(0)}원 - ${maxAmount.toStringAsFixed(0)}원'),
-              RangeSlider(
-                values: RangeValues(minAmount, maxAmount),
-                onChanged: (RangeValues values) {
-                  setState(() {
-                    minAmount = values.start;
-                    maxAmount = values.end;
-                  });
-                },
-                min: 0,
-                max: 1000000,
-                divisions: 100,
-                labels: RangeLabels(
-                  minAmount.toStringAsFixed(2),
-                  maxAmount.toStringAsFixed(2),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  ref.read(travelListProvider.notifier).ConditionAPI(
-                    startDateController.text,
-                    endDateController.text,
-                    maxAmount.toInt(),
-                    keywordController.text,
-                    context,
-                  );
-                  context.pop();
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(travelListProvider.notifier).ConditionAPI(
+                  startDateController.text,
+                  endDateController.text,
+                  maxAmount.toInt(),
+                  keywordController.text,
+                  context,
+                );
+                context.pop();
 
-                },
-                child: Text('검색'),
-              ),
-            ],
-          ),
+              },
+              child: Text('검색'),
+            ),
+          ],
         ),
       ),
     );
